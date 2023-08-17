65e49fcf94e62507e9b5fba7c308a2e472869e08
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-08-17 16:38:40 -0500

    [ADD] .env validation :knot: ✨

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-
+import { validationSchema } from './settings/validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema,
     }),
     DatabaseModule,
   ],
---
+++ src/settings/validation.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  HTTP_SERVER_PORT: Joi.string().required(),
+  SWAGGER_USER: Joi.string().required(),
+  SWAGGER_PASS: Joi.string().required(),
+  MGHOST: Joi.string().required(),
+  MGPORT: Joi.string().required(),
+  MGUSER: Joi.string().required(),
+  MGPASSWORD: Joi.string().required(),
+  MGDATABASE: Joi.string().required(),
+  MG_URI: Joi.string().required(),
+  CAT_API_KEY: Joi.string().required(),
+});
